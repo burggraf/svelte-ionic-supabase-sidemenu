de950d6873dadf37e5fa64ca3063fd652ab36a44
Mark Burggraf <markb@mantisbible.com>
2022-07-11 09:45:21 -0700

    redo login modal

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -1,25 +1,41 @@
 <script lang="ts">
-  import { modalController } from "$ionic/svelte";
+//import { modalController } from "$ionic/svelte";
+//import IonPage from "$lib/ionic/svelte/components/IonPage.svelte";
+import { IonContent } from "@ionic/core/components/ion-content";
+  import {
+    closeOutline,
+    lockOpenOutline,
+    lockClosedOutline
+  } from "ionicons/icons";
+
+  let email = '';
+  let password = '';
+  let providers: string[] = [];
+  let showModal = false;
+  let backdropDismiss = true;
+  let localUser: any = {};
+  let showLoading = false;
 </script>
 
 <ion-modal 
-isOpen={showModal} 
+is-open={showModal} 
 backdropDismiss={backdropDismiss}
 className='my-custom-class'>
   <ion-header>
     <ion-toolbar>
       <ion-buttons slot="start">
-        <ion-button color='primary' onClick={() => setShowModal(false)}>
+        <ion-button color='primary' onClick={() => {showModal = false;}}>
                 <ion-icon size='large' icon={closeOutline}></ion-icon>
-        </ion-button>
+            </ion-button>
     </ion-buttons>
       <ion-title>Sign In</ion-title>
     </ion-toolbar>
     <ion-buttons slot="start">
     </ion-buttons>
 </ion-header>
+<!-- <IonPage> -->
   <IonContent>
-      <ion-loading isOpen={showLoading} message={'Loading'} />
+    <ion-loading is-open={showLoading} message={'Loading'} />
 
     <ion-grid class="ion-padding" style={{maxWidth: '375px'}}>
         <ion-row>
@@ -31,23 +47,24 @@ className='my-custom-class'>
             <ion-col>
 
                 <ion-item className="loginItem" lines="none">
-                    <ion-input type="email"
-                    placeholder="Email" 
-                    onIonChange={e => setEmail(e.detail.value!)}
-                    value={email} className="loginInputBoxWithIcon">
-                    <ion-icon className="inputIcon" icon={mailOutline} slot="start" size="large" color="medium"></ion-icon>
+                    <ion-input 
+                        bind:value={email} 
+                        type="email" 
+                        placeholder="Email">
+                        <ion-icon class="inputIcon" icon={mailOutline} slot="start" size="large" color="medium"></ion-icon>
                     </ion-input>
+
                 </ion-item>
 
             </ion-col>
         </ion-row>
-        {!validateEmail(email) && email.length > 0 && 
+        {#if !validateEmail(email) && email.length > 0} 
             <ion-row>
                 <ion-col>
                     <ion-label color="danger"><b>Invalid email format</b></ion-label>
                 </ion-col>
             </ion-row>
-        }
+        {/if}
         <ion-row>
             <ion-col>
                 <ion-label>Password</ion-label>
@@ -58,9 +75,9 @@ className='my-custom-class'>
                 <ion-item className="loginItem" lines="none">
                     <ion-input type="password" 
                     placeholder="Password" 
-                    onIonChange={e => setPassword(e.detail.value!)}
-                    value={password} className="loginInputBoxWithIcon">
-                    <ion-icon className="inputIcon" 
+                    bind:value={password} 
+                    class="loginInputBoxWithIcon">
+                    <ion-icon  
                     icon={password.length ? lockOpenOutline: lockClosedOutline} 
                     slot="start" size="large" color="medium"></ion-icon>
                     </ion-input> 
@@ -70,87 +87,85 @@ className='my-custom-class'>
                 </div>
             </ion-col>
         </ion-row>
-        {password.length > 0 && password.length < 6 && 
+        {#if password.length > 0 && password.length < 6}
             <ion-row>
                 <ion-col>
                     <ion-label color="danger"><b>Password too short</b></ion-label>
                 </ion-col>
             </ion-row>
-        }
-        { !signUpMode &&
+        {/if}
+        {#if !signUpMode}
             <ion-row>
                 <ion-col>
-                    <IonButton expand="block" color="primary"
+                    <ion-button expand="block" color="primary"
                     disabled={!validateEmail(email) || password.length < 6}
                     onClick={signInWithEmail}>
                         <ion-icon icon={arrowForwardOutline} size="large" />&nbsp;&nbsp;
                         <b>Sign in with email</b>
-                    </IonButton>
+                    </ion-button>
                     <div onClick={toggleSignUpMode} className="ion-text-center" style={{paddingTop:'10px'}}>
                         <ion-label>Don't have an account? <b>Sign Up</b></ion-label>
                     </div>
                 </ion-col>
             </ion-row>
-        }
-        { signUpMode &&
+        {/if}
+        {#if signUpMode}
             <ion-row>
                 <ion-col>
-                        <IonButton expand="block" 
+                        <ion-button expand="block" 
                         disabled={!validateEmail(email) || password.length < 6}
                         onClick={signUp}>
                         <ion-icon icon={personAdd} size="large" />&nbsp;&nbsp;
-                        <b>Sign Up</b></IonButton>
+                        <b>Sign Up</b></ion-button>
                         <div onClick={toggleSignUpMode} className="ion-text-center" style={{paddingTop:'10px'}}>
                         <ion-label>Already have an account? <b>Sign In</b></ion-label>
                     </div>
                 </ion-col>
             </ion-row>
-        }
+        {/if}
         <ion-row>
             <ion-col>
                 <div className="ion-text-center" style={{marginBottom: '10px'}}>
                     <ion-label><b>or</b></ion-label>
                 </div>
-                <IonButton expand="block" 
+                <ion-button expand="block" 
                 disabled={!validateEmail(email) || password.length > 0}
                 onClick={sendMagicLink}>
                 <ion-icon icon={link} size="large" />&nbsp;&nbsp;
-                <b>Send Sign In Link</b></IonButton>                    
+                <b>Send Sign In Link</b></ion-button>                    
             </ion-col>
         </ion-row>
     </ion-grid>
-    { providers &&
+    {#if providers}
         <div className="ion-text-center">
         <ion-label>or, sign in with:</ion-label>
         </div>
-    }
+    {/if}
     <ion-grid class="ion-padding ion-text-center" style={{maxWidth: '375px'}}>
         <ion-row>
             <ion-col>
-                {providers?.map(provider => {
-                    return (
+                {#each providers as provider}
                     <ProviderSignInButton SUPABASE_URL={SUPABASE_URL} SUPABASE_KEY={SUPABASE_KEY} key={`provider-${provider}`} name={provider} color={logoColors[provider] || 'black'} />
-                    )
-                })} 
+                {/each}
         </ion-col>
         </ion-row>
     </ion-grid>    
 
   </IonContent>
 </ion-modal>
-{!localUser && (
+<IonContent>
+{#if !localUser}
     <ion-item lines='none' detail={false} onClick={() => setShowModal(true)}>
         <ion-icon slot='start' ios={logInOutline} md={logInSharp}></ion-icon>
         <div style={{width: '100%'}}>
-            <IonButton fill='outline' color='dark'
+            <ion-button fill='outline' color='dark'
                 size='small' expand='block' strong>
                 Sign In
-            </IonButton>
+            </ion-button>
         </div>
     </ion-item>
-)}
-{localUser && (
-    <>
+{/if}
+{#if localUser}
     <ion-item lines='none' detail={false} 
         style={{cursor: profileFunction ? 'pointer' : 'default'}}
         onClick={loadProfile}
@@ -163,15 +178,26 @@ className='my-custom-class'>
     <ion-item lines='none' detail={false} onClick={signOut}>
         <ion-icon slot='start' ios={logOutOutline} md={logOutSharp}></ion-icon>
         <div style={{width: '100%'}}>
-            <IonButton fill='outline' color='dark'
+            <ion-button fill='outline' color='dark'
                 size='small' expand='block' strong>
                 Sign Out
-            </IonButton>
+            </ion-button>
         </div>
     </ion-item>
-    </>
-)}
-
+{/if}
+</IonContent>
 
 <style>
+    .inputIcon {
+        margin-left: 10px;
+        margin-right: 10px
+    }
+    .loginInputBoxWithIcon {
+        height: 50px;
+        border: 1px solid rgb(212, 212, 212);
+        background-color: var(--ion-background-color) !important;
+        /* border-bottom: 1px solid hsl(0, 0%, 83%); */
+        border-radius: 5px;
+    }
+
 </style>
---
+++ src/components/LoginModal.svelte
@@ -0,0 +1,255 @@
+<script lang="ts">
+  import { modalController } from "$ionic/svelte";
+
+  import {
+    mailOutline,
+    closeOutline,
+    personAdd,
+    lockOpenOutline,
+    lockClosedOutline,
+    arrowForwardOutline,
+    link
+  } from "ionicons/icons";
+
+  export let firstName = undefined;
+  export let lastName = undefined;
+  export let middleInitial = undefined;
+
+  console.log("Received props", firstName, lastName, middleInitial);
+
+  const closeOverlay = () => {
+    modalController.dismiss({ data: Date.now() });
+  };
+
+  function handleEmailValue(event) {
+    email = event.target.value!;
+  }
+  function handlePasswordValue(event) {
+    password = event.target.value!;
+  }
+
+  // import { logoIonic, star, fingerPrint, ellipsisVertical } from "ionicons/icons";
+ 
+  // const clickAction = (event) => {
+  //   console.log("You clicked me!!", event.detail);
+  // };
+
+  const validateEmail = (email: string) => {
+    return true;
+  }
+  const resetPassword = () => {
+    console.log('NOT IMPLEMENTED');
+  }
+  const signInWithEmail = ()=> {
+    console.log('NOT IMPLEMENTED');
+  }
+  const signUp = () => {
+    console.log('NOT IMPLEMENTED');
+  }
+  const toggleSignUpMode = () => {
+    signUpMode = !signUpMode;
+  }
+  const sendMagicLink = () => {
+    console.log('NOT IMPLEMENTED');
+  }
+  let email = '';
+  let password = '';
+  let providers: string[] = [];
+  let showModal = false;
+  let backdropDismiss = true;
+  let localUser: any = {};
+  let showLoading = false;
+  let signUpMode = false;
+
+</script>
+
+<svelte:head>
+  <title>Ionic Companion - Modal Extra</title>
+</svelte:head>
+<ion-header translucent="true">
+  <ion-toolbar>
+    <ion-title>Modal Content</ion-title>
+    <ion-buttons slot="end">
+      <ion-button on:click={closeOverlay}>Close</ion-button>
+    </ion-buttons>
+  </ion-toolbar>
+</ion-header>
+
+<!-- <ion-content class="ion-padding"> -->
+  <ion-loading is-open={showLoading} message={'Loading'} />
+  <ion-grid class="ion-padding" style="max-width:375px">
+      <ion-row>
+          <ion-col>
+              <ion-label>Email Address</ion-label>
+          </ion-col>
+      </ion-row>
+      <ion-row>
+          <ion-col>
+
+              <ion-item class="loginItem" lines="none">
+                  <ion-input 
+                      on:ionChange={handleEmailValue}
+                      class="loginInputBoxWithIcon"
+                      type="email" 
+                      placeholder="Email">
+                      <ion-icon class="inputIcon" 
+                        icon={mailOutline} 
+                        slot="start" size="large" color="medium"></ion-icon>
+                  </ion-input>
+
+              </ion-item>
+
+          </ion-col>
+      </ion-row>
+      {#if !validateEmail(email) && email.length > 0} 
+          <ion-row>
+              <ion-col>
+                  <ion-label color="danger"><b>Invalid email format</b></ion-label>
+              </ion-col>
+          </ion-row>
+      {/if}
+      <ion-row>
+          <ion-col>
+              <ion-label>Password</ion-label>
+          </ion-col>
+      </ion-row>
+      <ion-row>
+          <ion-col>
+              <ion-item class="loginItem" lines="none">
+                  <ion-input type="password" 
+                  placeholder="Password" 
+                  on:ionChange={handlePasswordValue}
+                  class="loginInputBoxWithIcon">
+                  <ion-icon class="inputIcon"
+                    icon={password.length ? lockOpenOutline: lockClosedOutline} 
+                  slot="start" size="large" color="medium"></ion-icon>
+                  </ion-input> 
+              </ion-item>
+              <div onClick={resetPassword} 
+                    class="ion-text-right" 
+                    style="padding-top:10px">
+                  <ion-label><b>Forgot password?</b></ion-label>
+              </div>
+          </ion-col>
+      </ion-row>
+      {#if password.length > 0 && password.length < 6}
+          <ion-row>
+              <ion-col>
+                  <ion-label color="danger"><b>Password too short</b></ion-label>
+              </ion-col>
+          </ion-row>
+      {/if}
+      {#if !signUpMode}
+          <ion-row>
+              <ion-col>
+                  <ion-button expand="block" color="primary"
+                  disabled={!validateEmail(email) || password.length < 6}
+                  onClick={signInWithEmail}>
+                      <ion-icon icon={arrowForwardOutline} size="large" />&nbsp;&nbsp;
+                      <b>Sign in with email</b>
+                  </ion-button>
+                  <div onClick={toggleSignUpMode} 
+                      class="ion-text-center" 
+                      style="padding-top:10px">
+                      <ion-label>Don't have an account? <b>Sign Up</b></ion-label>
+                  </div>
+              </ion-col>
+          </ion-row>
+      {/if}
+      {#if signUpMode}
+          <ion-row>
+              <ion-col>
+                      <ion-button expand="block" 
+                      disabled={!validateEmail(email) || password.length < 6}
+                      onClick={signUp}>
+                      <ion-icon icon={personAdd} size="large" />&nbsp;&nbsp;
+                      <b>Sign Up</b></ion-button>
+                      <div onClick={toggleSignUpMode} 
+                      class="ion-text-center" 
+                      style="padding-top:10px">
+                      <ion-label>Already have an account? <b>Sign In</b></ion-label>
+                  </div>
+              </ion-col>
+          </ion-row>
+      {/if}
+      <ion-row>
+          <ion-col>
+              <div class="ion-text-center" style="margin-bottom:10px">
+                  <ion-label><b>or</b></ion-label>
+              </div>
+              <ion-button expand="block" 
+              disabled={!validateEmail(email) || password.length > 0}
+              onClick={sendMagicLink}>
+              <ion-icon icon={link} size="large" />&nbsp;&nbsp;
+              <b>Send Sign In Link</b></ion-button>                    
+          </ion-col>
+      </ion-row>
+  </ion-grid>
+  {#if providers}
+      <div class="ion-text-center">
+      <ion-label>or, sign in with:</ion-label>
+      </div>
+  {/if}
+  <ion-grid class="ion-padding ion-text-center" style="max-width:375px">
+      <ion-row>
+          <ion-col>
+              {#each providers as provider}
+                  <!-- <ProviderSignInButton SUPABASE_URL={SUPABASE_URL} SUPABASE_KEY={SUPABASE_KEY} key={`provider-${provider}`} name={provider} color={logoColors[provider] || 'black'} /> -->
+              {/each}
+      </ion-col>
+      </ion-row>
+  </ion-grid>    
+<!-- </ion-content> -->
+
+<style>
+input:-webkit-autofill {
+    /* height: 0px; */
+    /* -webkit-box-shadow:0 0 0 10px var(--ion-color-FORCECLEAR) inset; */
+    -webkit-text-fill-color: var(--ion-color-FORCEDARK);
+    font-weight: 500px;
+}
+
+input:-webkit-autofill:focus {
+    /* height: 0px; */
+    /* -webkit-box-shadow: 0 0 0 10px var(--ion-color-FORCECLEAR) inset; */
+    -webkit-text-fill-color: var(--ion-color-FORCEDARK);
+    font-weight: 500px;
+} 
+
+
+.inputIcon {
+    margin-left: 10px;
+    margin-right: 10px
+}
+
+.loginItem {
+    --padding-start: 0px;
+    --padding-end: 0px;
+    --inner-padding-end: 0px;
+}
+.loginInputBoxWithIcon {
+    height: 50px;
+    border: 1px solid rgb(212, 212, 212);
+    background-color: var(--ion-background-color) !important;
+    /* border-bottom: 1px solid hsl(0, 0%, 83%); */
+    border-radius: 5px;
+}
+.toast {
+    font-weight: bold;
+}
+.flex-container {
+    display: flex;
+    display: -webkit-flex;
+    display: -moz-flex;
+    flex-flow: row wrap;
+    -webkit-flex-flow: row wrap;
+    -moz-flex-flow: row wrap;
+    justify-content: center;
+
+    /* flex-wrap: wrap; */
+    /* border: 1px solid; */
+
+    /* margin-right: 10px;
+    margin-left: 10px; */
+}
+</style>
